06355fbe5853d0f7b9586ea45f41423ad0125d3e
Nicolás Trujillo <tnicolas@uninorte.edu.co>
2020-07-29 17:21:10 -0500

    Segundo Commit
    Subiendo el build

---
+++ build/precache-manifest.84f9f854ab200de7edf9737192ca7266.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7cbf85d280611d894b2c1973146d76",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a26f99a82a563c4f45be",
+    "url": "/static/css/main.28da8ee0.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a26f99a82a563c4f45be",
+    "url": "/static/js/main.75b72eee.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
